0bb4d0d369fa1dd7b63c51ff340263f2bd7e87e6
Diego Andriano <andrianodna@gmail.com>
2024-03-26 11:17:05 -0300

    Presente escritura

---
+++ app/Actions/Plantillas/PresenteEscritura.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Actions\Plantillas;
+
+use App\Models\SubTag;
+use App\Models\Tag;
+use App\Models\Todo;
+use Carbon\Carbon;
+
+class Leetcode implements IPlantilla
+{
+    public function execute(): array
+    {
+        Todo::create([
+            'name' => 'Presente escritura',
+            'description' => "",
+            'points' => 1,
+            'priority' => 2,
+            'tag_id' => Tag::where('name', 'Misc')->first()->id,
+            'sub_tag_id' => SubTag::where('name', 'Escritura')->first()->id,
+            'state' => 'done',
+            'done_at' => Carbon::now(),
+            'doing_at' => Carbon::now()->subMinutes(30),
+            'highlight' => false,
+            'user_id' => auth()->user()->id,
+        ]);
+        return ['error' => false];
+    }
+}
